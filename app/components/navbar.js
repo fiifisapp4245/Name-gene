7da39e98973da6a90de3cdc04654d72d49f30d08
Fiifi Appiah <fiifisapp4245@gmail.com>
2023-11-26 18:08:44 +0000

    styled the home page

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,30 +1,55 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div className="bg-gray-800 text-white p-4 flex justify-between items-center">
-      <div className="text-xl font-bold">
-        {/* You can replace the text below with an actual logo */}
-        <Link href="/">
-          Logo
-        </Link>
+    <nav className="bg-white p-2 fixed w-full top-0 z-10">
+      <div className="container mx-auto flex items-center justify-between">
+        <div className="text-xl font-bold text-blue-600">
+          <Link href="/">NameGene</Link>
+        </div>
+        <div className="lg:hidden">
+          <button
+            onClick={() => setMenuOpen(!menuOpen)}
+            className="text-black focus:outline-none"
+          >
+            Menu
+          </button>
+        </div>
+        <div
+          className={`${
+            menuOpen ? "block" : "hidden"
+          } lg:flex lg:items-center`}
+        >
+          <Link
+            href="/"
+            className="block lg:inline-block p-2 text-black hover:text-blue-600"
+          >
+            Home
+          </Link>
+          <Link
+            href="/about"
+            className="block lg:inline-block p-2 text-black hover:text-blue-600"
+          >
+            About Us
+          </Link>
+          <Link
+            href="/contact"
+            className="block lg:inline-block p-2 text-black hover:text-blue-600"
+          >
+            Contact
+          </Link>
+        </div>
       </div>
-      <div className="flex gap-4">
-        <Link href="/" className="hover:text-gray-300">
-          Home
-        </Link>
-        <Link href="/about" className="hover:text-gray-300">
-          About Us
-        </Link>
-        <Link href="/contact" className="hover:text-gray-300">
-          Contact{" "}
-        </Link>
-      </div>
-    </div>
+    </nav>
   );
 };
 
 export default Navbar;
+
+
+
--- app/home/page.js
+++ app/home/page.js
@@ -13,47 +13,61 @@ const HomePage = () => {
     event.preventDefault();
 
     // Use the industryType as a parameter in the URL
-    push(`/generate/${industryType}?keyword=${keyword}&namingStyle=${namingStyle}`);
-};
+    push(
+      `/generate/${industryType}?keyword=${keyword}&namingStyle=${namingStyle}`
+    );
+  };
 
   return (
-    <div>
-      <header>
-        <h1 className="text-5xl font-bold mt-2">
-          Craft Your Unique Identity
-        </h1>
-        <p className="">
-          Choosing the right identity is pivotal. Let us guide you through the
-          process of crafting a name that aligns perfectly with your brand
-          ethos. Your input matters, and we're here to make it count.
-        </p>
+    <div className="min-h-screen flex flex-col items-center bg-white">
+      <header className="text-center m-10 w-full h-20">
+        <div className="max-w-screen-md mx-auto">
+          <h1 className="text-4xl md:text-6xl font-bold mt-2 text-blue-600">
+            Craft Your Unique Identity
+          </h1>
+          <p className="mt-4 text-gray-600 max-w-screen-md mx-auto">
+            Choosing the right identity is pivotal. Let us guide you through the
+            process of crafting a name that aligns perfectly with your brand
+            ethos. Your input matters, and we're here to make it count.
+          </p>
+        </div>
       </header>
-      <main>
-        <form onSubmit={handleSubmit}>
-          <label>
-            keyword
+      <main className="mt-8 w-full">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-white p-8 rounded-md flex flex-wrap justify-center"
+        >
+          <div className="flex flex-col items-stretch mb-4 mx-2">
+            <label className="mb-2 text-blue-600 font-semibold">Keyword</label>
             <input
               type="text"
-              placeholder="keywords"
+              placeholder="Keywords"
               value={keyword}
               onChange={(e) => setKeyword(e.target.value)}
+              className="flex-grow p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
             />
-          </label>
-          <label>
-            Industry Type
+          </div>
+          <div className="flex flex-col items-stretch mb-4 mx-2">
+            <label className="mb-2 text-blue-600 font-semibold">
+              Industry Type
+            </label>
             <input
               type="text"
-              placeholder="Industry type ..."
+              placeholder="Industry type..."
               value={industryType}
               onChange={(e) => setIndustryType(e.target.value)}
+              className="flex-grow p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
             />
-          </label>
-          <label>
-            Naming Style
+          </div>
+          <div className="flex flex-col items-stretch mb-4 mx-2">
+            <label className="mb-2 text-blue-600 font-semibold">
+              Naming Style
+            </label>
             <select
               value={namingStyle}
               onChange={(e) => setNamingStyle(e.target.value)}
-            >              
+              className="flex-grow p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+            >
               <option value="modern">Modern</option>
               <option value="traditional">Traditional</option>
               <option value="innovative">Innovative</option>
@@ -63,14 +77,70 @@ const HomePage = () => {
               <option value="luxury">Luxury</option>
               <option value="global">Global</option>
             </select>
-          </label>
-          <button type="submit" aria-label="Generate names">
+          </div>
+          <button
+            type="submit"
+            aria-label="Generate names"
+            className="w-full p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600 mx-2"
+          >
             Generate names
           </button>
         </form>
+        <section className="w-full p-20 mt-8 bg-blue-600 flex justify-center">
+          {/* Card 1 */}
+          <div className="max-w-xs mx-2 bg-white p-2 rounded-md shadow-md">
+            <h2 className="text-base font-semibold text-blue-600 mb-2">
+              Step 1: Enter Criteria
+            </h2>
+            <p className="text-sm text-gray-600">
+              Start by providing us with some key information. Enter the
+              industry type your business belongs to and your preferred naming
+              style. The more specific you are, the more tailored our
+              suggestions will be.
+            </p>
+          </div>
+
+          {/* Card 2 */}
+          <div className="max-w-xs mx-2 bg-white p-2 rounded-md shadow-md">
+            <h2 className="text-base font-semibold text-blue-600 mb-2">
+              Step 2: Name Generation
+            </h2>
+            <p className="text-sm text-gray-600">
+              Once you've entered your criteria, our advanced OpenAI model gets
+              to work. It processes your inputs to generate a diverse set of
+              potential names that align with your industry and naming style
+              preferences.
+            </p>
+          </div>
+
+          {/* Card 3 */}
+          <div className="max-w-xs mx-2 bg-white p-2 rounded-md shadow-md">
+            <h2 className="text-base font-semibold text-blue-600 mb-2">
+              Step 3: Check Trademark
+            </h2>
+            <p className="text-sm text-gray-600">
+              We take care of legal considerations. We check the generated names
+              against trademark databases to ensure they are not already
+              trademarked. This step is crucial to help you avoid any legal
+              complications down the road.
+            </p>
+          </div>
+
+          {/* Card 4 */}
+          <div className="max-w-xs mx-2 bg-white p-2 rounded-md shadow-md">
+            <h2 className="text-base font-semibold text-blue-600 mb-2">
+              Step 4: Check Domain Availability
+            </h2>
+            <p className="text-sm text-gray-600">
+              We check the availability of domain names corresponding to the
+              selected names. A strong online presence is essential, and we want
+              to make sure your chosen names come with available domains.
+            </p>
+          </div>
+        </section>
       </main>
     </div>
   );
-}
+};
 
 export default HomePage;
